79f815a92edf7dbd35a93fe54f598d966e5bf3cb
GK-Coding <gio.kane1124@gmail.com>
2024-09-13 13:35:39 -0400

    Fixing Bug

--- core.js
+++ core.js
@@ -4,10 +4,18 @@ const menuBtn = document.getElementById("menu-btn");
 themeSwitch.addEventListener("click", (e) => {
     const themeSwitchKnob = document.getElementById("theme-switch-knob");
     const elementsToChange = [...document.getElementsByClassName("receives-formal"), ...document.getElementsByClassName("swiper-slide")];
+    const contactBtn = document.getElementById("contactBtn");
+    const planningBtn = document.getElementById("planningBtn");
     themeSwitch.classList.toggle("active");
     themeSwitchKnob.classList.toggle("active");
     for (const element of elementsToChange) {
         element.classList.toggle("formal");
+    }
+    if (contactBtn) {
+        contactBtn.innerText = contactBtn.innerText === "Get the convo started" ? "Get in touch" : "Get the convo started"
+    }
+    if (planningBtn) {
+        planningBtn.innerText = planningBtn.innerText === "Ask us how we do it" ? "Contact us" : "Ask us how we do it"
     }
 });
 
