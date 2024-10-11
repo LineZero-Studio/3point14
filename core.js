4765a4c8aa4a875201d54cc06f6fb8174f9840a5
GK-Coding <gio.kane1124@gmail.com>
2024-10-11 01:16:09 -0400

    Transition Fix

--- core.js
+++ core.js
@@ -3,54 +3,56 @@ const menuBtn = document.getElementById("menu-btn");
 const servicesMobileLink = document.getElementById("servicesMobileLink");
 
 function setCookie(cookieName, cookieVal, expiry) {
-  var d = new Date();
-  d.setTime(d.getTime() + (expiry * 24 * 60 * 60 * 1000));
-  var expires = "expires=" + d.toUTCString();
-  document.cookie = cookieName + "=" + cookieVal + ";" + expires + ";path=/";
+    var d = new Date();
+    d.setTime(d.getTime() + (expiry * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieVal + ";" + expires + ";path=/";
 }
 
 function getCookie(cookieName) {
-  var name = cookieName + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
 
 function checkCookie() {
-  var sitemode = getCookie("sitemode");
-  const elementsToChange = [...document.getElementsByClassName("receives-formal"), ...document.getElementsByClassName("swiper-slide")];
-  for (const element of elementsToChange) {
-    if (element.classList.contains("receives-formal")) {
-        element.classList.add("loaded");
+    var sitemode = getCookie("sitemode");
+    const elementsToChange = [...document.getElementsByClassName("receives-formal"), ...document.getElementsByClassName("swiper-slide")];
+    setTimeout(() => {
+    for (const element of elementsToChange) {
+        if (element.classList.contains("receives-formal")) {
+            element.classList.add("loaded");
+        }
+    } 
+    }, 0);
+    if (sitemode != 0) {
+        if (sitemode == "formal") {
+            const themeSwitchKnob = document.getElementById("theme-switch-knob");
+            const contactBtn = document.getElementById("contactBtn");
+            const planningBtn = document.getElementById("planningBtn");
+            themeSwitch.classList.toggle("active");
+            themeSwitchKnob.classList.toggle("active");
+            for (const element of elementsToChange) {
+                element.classList.toggle("formal");
+            }
+            if (contactBtn) {
+                contactBtn.innerText = contactBtn.innerText === "Get the convo started" ? "Get in touch" : "Get the convo started"
+            }
+            if (planningBtn) {
+                planningBtn.innerText = planningBtn.innerText === "Ask us how we do it" ? "Contact us" : "Ask us how we do it"
+            }
+        }
     }
-  }
-  if (sitemode != 0) {
-    if (sitemode == "formal") {
-        const themeSwitchKnob = document.getElementById("theme-switch-knob");
-        const contactBtn = document.getElementById("contactBtn");
-        const planningBtn = document.getElementById("planningBtn");
-        themeSwitch.classList.toggle("active");
-        themeSwitchKnob.classList.toggle("active");
-        for (const element of elementsToChange) {
-            element.classList.toggle("formal");
-        }
-        if (contactBtn) {
-            contactBtn.innerText = contactBtn.innerText === "Get the convo started" ? "Get in touch" : "Get the convo started"
-        }
-        if (planningBtn) {
-            planningBtn.innerText = planningBtn.innerText === "Ask us how we do it" ? "Contact us" : "Ask us how we do it"
-        }
-    }
-  }
 }
 
 document.addEventListener("DOMContentLoaded", checkCookie);
